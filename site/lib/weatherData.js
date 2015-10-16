ea03f05400865cb89c475addb4b645928f03fae0
guotong <guotong159@gmail.com>
2015-10-16 18:31:45 +0800

    handlebars 模板研究

---
+++ site/lib/weatherData.js
@@ -0,0 +1,30 @@
+/**
+ * 获取天气信息模块
+ * @authors guotong
+ * @date    2015-10-16
+ */
+exports.getWeatherData = function() {
+    return {
+        locations: [
+            {
+                name: 'Protland',
+                forecastUrl: 'http:// www.Wunderground.com/US/OR/Protland.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+                weather: 'Overcast',
+                temp: '54.1 F (12.3 C)',
+            }, {
+                name: 'Bend',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+                weather: 'Partly Cloudy',
+                temp: '55.0 F (12.8 C)',
+            }, {
+                name: 'Manzanita',
+                forecastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+                iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+                weather: 'Light Rain',
+                temp: '55.0 F (12.8 C)',
+            }, 
+        ],
+    };
+};
--- site/meadowlark.js
+++ site/meadowlark.js
@@ -10,6 +10,8 @@ var handlebars = require('express-handlebars');
 
 // 引入幸运饼干模块
 var fortune = require('./lib/fortune.js');
+// 引入获取天气数据
+var weatherData = require('./lib/weatherData.js');
 
 app.set('port',process.env.PORT || 3000);
 
@@ -17,10 +19,25 @@ app.use(express.static(__dirname + '/public'));
 
 
 // set view engine
-app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.engine('handlebars', handlebars({ 
+	defaultLayout: 'main',
+	helpers : {
+		section: function(name, options){
+            if(!this._sections) this._sections = {};
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+	}
+
+}));
 app.set('view engine','handlebars');
 
 // 测试
+app.use(function (req,res,next){
+	if (!res.locals.partials) res.locals.partials = {};
+	res.locals.partials.weatherContext = weatherData.getWeatherData();
+	next();
+})
 app.use(function (req,res,next){
 	res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
 	next();
@@ -46,6 +63,21 @@ app.get('/tours/request-group-rate',function (req,res){
 	res.render('tours/request-group-rate');
 });
 
+app.get('/jquerytest', function (req,res){
+	res.render('jquerytest');
+});
+app.get('/nursery-rhyme',function (req,res){
+	res.render('nursery-rhyme');
+});
+app.get('/data/nursery-rhyme',function (req,res){
+	res.json({
+		animal:'squirrl',
+		bodyPart : 'tail',
+		adjective : 'bushy',
+		noun : 'heck'
+	});
+});
+
 // setting 404 page
 app.use(function (req,res){
 	res.status(404);
@@ -63,3 +95,18 @@ app.use(function (req,res){
 app.listen(app.get('port'),function(){
 	console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl+C to terminate.');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
